5c40891b287c8d28750b684eeb00fa42325b127c
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-01 23:11:20 +0200

    Day 1: Report Repair : Part1

---
+++ AdventOfCode2020/Day 1 - Report Repair/ReportRepair.fs
@@ -0,0 +1,16 @@
+﻿module ReportRepair
+
+let sumTo2020 entries =
+    let rec test xs x =
+        let l = xs |> List.length
+        let head = xs |> List.head
+
+        match (head + x), l with
+        | 2020, _ -> Some (x, head)
+        | _, 1 -> None
+        | _, _ -> test (xs |> List.tail) x
+
+    entries
+    |> List.map (fun x -> test (List.except [x] entries) x)
+    |> List.choose id
+    |> List.head
