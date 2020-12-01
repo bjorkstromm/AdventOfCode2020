b330c0b760afb2017b30f66ecd3a0f946d6bc37b
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-01 23:46:09 +0200

    Day 1 - Part II

--- AdventOfCode2020/Day 1 - Report Repair/ReportRepair.fs
+++ AdventOfCode2020/Day 1 - Report Repair/ReportRepair.fs
@@ -14,3 +14,27 @@ let sumTo2020 entries =
     |> List.map test
     |> List.choose id
     |> List.head
+
+let sumTo2020part2 entries =
+    let rec test (x, y, xs) =
+        let head = xs |> List.head
+
+        match (head + x + y, xs |> List.tail) with
+        | (2020, _) -> Some (x, y, head)
+        | (_, []) -> None
+        | (_, tail) -> test (x, y, tail)
+
+    let rec test2 (x, xs) =
+        let head = xs |> List.head
+        let r = test (x, head, entries |> List.except [x; head])
+
+        match (r, xs |> List.tail) with
+        | (Some r, _) -> Some r
+        | (_, []) -> None
+        | (_, tail) -> test2 (x, tail)
+
+    entries
+    |> List.map (fun x -> (x, entries |> List.except [x]))
+    |> List.map test2
+    |> List.choose id
+    |> List.head
