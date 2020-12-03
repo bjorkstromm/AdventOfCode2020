b2cab644abf47727b57d5e72f13963185a354d30
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-03 22:21:01 +0200

    Day 3

---
+++ AdventOfCode2020/Day 3 - Toboggan Trajectory/Trajectory.fs
@@ -0,0 +1,21 @@
+﻿module Trajectory
+
+let traverse (expression : string[]) (right : int, down : int) =
+    let map = expression
+            |> Array.map (fun str -> str.ToCharArray())
+
+    let xMax = map.[0].Length
+    let yMax = expression.Length
+
+    let rec move (x, y) trees =
+        let cnt = if map.[y].[x] = '#' then trees + 1 else trees
+        let newX = (x + right) % xMax
+        let newY = y + down
+
+        if newY >= yMax then
+            cnt
+        else
+            move (newX, newY) cnt
+
+    move (0, 0) 0
+
