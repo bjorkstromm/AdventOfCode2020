049ab18f2957ec17fb6aa1626605daf0babedb57
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-06 21:55:36 +0200

    Day 6 - Part I

---
+++ AdventOfCode2020/Day 6 - Custom Customs/Customs.fs
@@ -0,0 +1,17 @@
+﻿module Customs
+
+open System
+
+let scan (input : string) =
+    let scanPerson (p : string) =
+        p.ToCharArray()
+
+    let scanGroup (g : string) =
+        g.Split(Environment.NewLine)
+        |> Array.map scanPerson
+        |> Array.concat
+        |> Array.distinct
+
+    input.Split(Environment.NewLine + Environment.NewLine)
+    |> Array.map scanGroup
+    |> Array.map Array.length
