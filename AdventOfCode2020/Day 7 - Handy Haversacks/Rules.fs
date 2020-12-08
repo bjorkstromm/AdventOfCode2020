3b73131bead88e62014473f0babf3558203223a2
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-08 09:53:00 +0200

    Day7 - Part II (Not a good solution, I wan't to use fold)

--- AdventOfCode2020/Day 7 - Handy Haversacks/Rules.fs
+++ AdventOfCode2020/Day 7 - Handy Haversacks/Rules.fs
@@ -1,6 +1,8 @@
 ﻿module Rules
 
 open System
+open System.Collections
+open System.Collections.Generic
 open System.Text.RegularExpressions
 
 let RegexMatches (pattern : string) (input : string) =
@@ -34,3 +36,31 @@ let processRules (ruleExp : string) (target : string) =
     |> Seq.map fst
     |> Seq.filter contains
     |> Seq.toArray
+
+let countBags (ruleExp : string) (target : string) =
+    let parseRule (ruleExp : string) =
+        let tokens = ruleExp.Split(" bags contain ")
+        let bag = tokens |> Array.head
+        let matches = RegexMatches @"(\d+\s.*?)\sbag" (tokens |> Array.last)
+                      |> Array.map (fun x -> let index = x.IndexOf(" ")
+                                             (x.Substring(0, index) |> int, x.Substring(index + 1)))
+        (bag, matches)
+
+    let parseRules (ruleExp : string) =
+        ruleExp.Split(Environment.NewLine)
+        |> Array.map parseRule
+        |> Map.ofArray
+
+    let rules = parseRules ruleExp
+
+    let stack = Stack<(int * string)>()
+    stack.Push((1, target))
+    let mutable total = 0
+
+    while stack.Count > 0 do
+        let (n, c) = stack.Pop()
+        total <- (total + n)
+        for (n_child, c_child) in rules.[c] do
+            stack.Push((n * n_child, c_child))
+
+    total - 1
