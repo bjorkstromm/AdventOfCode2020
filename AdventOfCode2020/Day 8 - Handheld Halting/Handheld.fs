40c115a4f59176fb31273c593b20044af4d33139
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-08 10:16:26 +0200

    Day 8 - Part II

---
+++ AdventOfCode2020/Day 8 - Handheld Halting/Handheld.fs
@@ -0,0 +1,20 @@
+﻿module Handheld
+
+open System
+
+let execute (bootCode : string) =
+    let instructions = bootCode.Split(Environment.NewLine)
+                       |> Array.map (fun str -> str.Split(" "))
+                       |> Array.map (fun arr -> (arr |> Array.head, arr |> Array.last |> int))
+
+    let rec run (acc, index, executed) =
+        if executed |> List.contains index then
+            acc
+        else
+            match (instructions.[index], index + 1, index :: executed) with
+            | (("nop", _), i, e) -> run (acc, i, e)
+            | (("acc", a), i, e) -> run (acc + a, i, e)
+            | (("jmp", a), _, e) -> run (acc, index + a, e)
+            | _ -> failwith "Invalid operation"
+
+    run (0, 0, [])
