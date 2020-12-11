6adb6c6b02521016207e071024ba594d63efb402
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-11 23:53:38 +0200

    Day 11 - Part 2

--- AdventOfCode2020/Day 11 - Seating System/SeatMap.fs
+++ AdventOfCode2020/Day 11 - Seating System/SeatMap.fs
@@ -50,3 +50,84 @@ let simulate (exp : string) =
     |> Array.map (fun s -> s.ToCharArray())
     |> array2D
     |> sim
+
+type Direction =
+    | NW
+    | N
+    | NE
+    | W
+    | E
+    | SW
+    | S
+    | SE
+
+let allDirections = [|
+    NW
+    N
+    NE
+    W
+    E
+    SW
+    S
+    SE
+|]
+
+let simulate2 (exp : string) =
+    let occupiedNearby (x,y) (map :char [,]) =
+        let xLen = map |> Array2D.length2
+        let yLen = map |> Array2D.length1
+        let rec findNearby (x,y) direction =
+            let (newX, newY) =
+                match direction with
+                | NW -> (x-1, y-1)
+                | N -> (x, y-1)
+                | NE -> (x+1,y-1)
+                | W -> (x-1, y)
+                | E -> (x+1, y)
+                | SW -> (x-1, y+1)
+                | S -> (x, y+1)
+                | SE -> (x+1, y+1)
+            match (newX, newY) with
+            | (-1, _) -> '.'
+            | (_, -1) -> '.'
+            | (n, _) when n = xLen -> '.'
+            | (_, n) when n = yLen -> '.'
+            | _ ->
+                match map.[newY,newX] with
+                | '.' -> findNearby (newX, newY) direction
+                | c -> c
+        allDirections
+        |> Array.map (fun dir -> findNearby (x,y) dir)
+        |> Array.filter (fun c -> c = '#')
+        |> Array.length
+
+    let getState (x,y) (map : char [,]) =
+        match map.[y,x] with
+        | c when c = 'L' || c = '#' ->
+            match (c, occupiedNearby (x,y) map) with
+            | ('L', 0) -> '#'
+            | ('#', n) when n >= 5 -> 'L'
+            | (c, _) -> c
+        | c -> c
+
+    let rec sim (currentMap : char [,]) =
+        let newMap = Array2D.copy currentMap
+        let xLen = newMap |> Array2D.length2
+        let yLen = newMap |> Array2D.length1
+        [|0..xLen-1|]
+        |> Array.Parallel.iter (fun x ->
+            [|0..yLen-1|] |> Array.Parallel.iter (fun y ->
+                 newMap.[y,x] <- getState (x,y) currentMap))
+        match newMap = currentMap with
+        | false -> sim newMap
+        | _ ->
+            newMap
+            |> Seq.cast<char>
+            |> Seq.toArray
+            |> Seq.filter (fun c -> c = '#')
+            |> Seq.length
+
+    exp.Split(System.Environment.NewLine)
+    |> Array.map (fun s -> s.ToCharArray())
+    |> array2D
+    |> sim
