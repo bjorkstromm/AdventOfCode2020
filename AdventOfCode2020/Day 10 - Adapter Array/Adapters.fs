83120e7d27a610be98b14bf5c532f7497c2399a2
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-12 00:16:35 +0200

    Day 10 - Part II

--- AdventOfCode2020/Day 10 - Adapter Array/Adapters.fs
+++ AdventOfCode2020/Day 10 - Adapter Array/Adapters.fs
@@ -1,4 +1,5 @@
 ﻿module Adapters
+open System.Collections.Generic
 
 let joltage (adapters : int array) =
     let rec count (x, y, z) arr =
@@ -19,20 +20,30 @@ let joltage (adapters : int array) =
     |> Array.toList
     |> count (1,1,1)
 
+// Solution below borrowed from CompositionalIT
+// https://github.com/CompositionalIT/advent-of-code-2020/blob/6aedcf360cc400ecb58c5ae55317d6c916b62376/Solutions.fsx#L394-L413
+
 let combinations (adapters : int array) =
-    let rec count (curr : int) (tail : int list) : int list list =
-        match tail with
-        | [] -> [[curr]]
-        | _ ->
-            tail
-            |> List.mapi (fun i x -> (i,x))
-            |> List.filter (fun (_,x) -> 0 < (x - curr) && (x - curr) < 4 )
-            |> List.map (fun (i, x) -> count x (tail.[i..]))
-            |> List.map (fun x -> x |> List.map (fun y -> curr::y))
-            |> List.concat
+    let adapters = adapters
+                   |> Array.append [|0; (Array.max adapters)+3|]
+                   |> Array.sort
+
+    let nextLookup =
+        adapters
+        |> Array.map(fun a ->
+            let p = adapters |> Array.filter(fun r -> r > a && r <= a + 3)
+            (a, p))
+        |> Map
+
+    let rec calculateSize (nextLookup : Map<_,_>) (cache : IDictionary<_,_>) current =
+        match nextLookup.[current] with
+        | [||] -> 1UL
+        | nextNumbers ->
+            nextNumbers
+            |> Array.sumBy (fun number ->
+                if not (cache.ContainsKey number) then
+                    cache.Add (number, calculateSize nextLookup cache number)
+                cache.[number])
 
 
-    adapters
-    |> Array.sort
-    |> Array.toList
-    |> count 0
+    calculateSize nextLookup (Dictionary()) adapters.[0]
