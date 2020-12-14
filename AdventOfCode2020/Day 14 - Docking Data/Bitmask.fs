83ef6be1034603c5d9f8fa4aa22e3199455b446d
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-15 00:00:45 +0200

    Day 14 - Part II - WIP...

--- AdventOfCode2020/Day 14 - Docking Data/Bitmask.fs
+++ AdventOfCode2020/Day 14 - Docking Data/Bitmask.fs
@@ -9,7 +9,6 @@ type Instruction =
     | Mask of Mask
     | Mem of Mem
 
-
 let applyMask value mask =
     let rec apply arr (value : int64) =
         match arr with
@@ -51,5 +50,61 @@ let apply (exp : string) =
     let (mem, _) =
         instructions
         |> Array.fold folder (Dictionary<int, int64>(), [||])
+
+    mem.Values |> Seq.sum
+
+// Part 2
+
+type Mem2 = int64 * int64
+
+type Instruction2 =
+    | Mask of string
+    | Mem of Mem2
+
+let applyMask2 address (mask : string) =
+    let rec intToBinary i =
+        match i with
+        | 0 | 1 -> string i
+        | _ ->
+            let bit = string (i % 2)
+            (intToBinary (i / 2)) + bit
+
+    let folder (mask : char array) (c : char) =
+        let index = mask |> Array.findIndex ((=) 'X')
+        Array.concat [| mask.[..index-1] ; [|c|] ; mask.[index+1..] |]
+
+    let combinations = mask |> Seq.filter ((=) 'X') |> Seq.length
+
+    // TODO: Apply to address first.
+
+    [|0..combinations-1|]
+    |> Array.map (fun i ->
+        let arr = (intToBinary i).ToCharArray()
+        arr |> Array.fold folder (mask.ToCharArray()))
+    |> Array.map string
+    |> Array.map (fun str -> System.Convert.ToInt64(str, 2))
+
+let apply2 (exp : string) =
+    let instructions =
+        exp.Split(Environment.NewLine)
+        |> Array.map (fun str ->
+            let tokens = str.Split(" = ")
+            match tokens.[0] with
+            | "mask" -> Mask tokens.[1]
+            | instr when instr.StartsWith("mem[") ->
+                Mem (instr.Substring(4, instr.Length - 5) |> int64, tokens.[1] |> int64)
+            | instr -> failwithf "Invalid instruction %s" instr)
+
+    let folder ((mem, mask) : (Dictionary<int64, int64> * string)) (instruction : Instruction2) =
+        match instruction with
+        | Mask newMask -> (mem, newMask)
+        | Mem (address, value) ->
+            let addresses = applyMask2 address mask 
+            addresses |> Array.iter (fun i -> mem.[i] <- value)
+            (mem, mask)
+
+    let (mem, _) =
+        instructions
+        |> Array.fold folder (Dictionary<int64, int64>(), "")
 
     mem.Values |> Seq.sum
--- AdventOfCode2020/Day 14 - Docking Data/BitmaskTests.fs
+++ AdventOfCode2020/Day 14 - Docking Data/BitmaskTests.fs
@@ -567,3 +567,14 @@ mem[50713] = 7725506"
 
         let r = apply input
         output.WriteLine("{0}", r)
+
+    [<Fact>]
+    member __.``Example Data - Part II`` () =
+        let input = "mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"
+
+        let r = apply input
+
+        r |> should equal 208L
