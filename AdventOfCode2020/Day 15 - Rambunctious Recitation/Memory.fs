177657e33823ddb11906051ab7414b251ddbdb8a
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-15 10:12:38 +0200

    Day 15 - Part I

---
+++ AdventOfCode2020/Day 15 - Rambunctious Recitation/Memory.fs
@@ -0,0 +1,25 @@
+﻿module Memory
+
+let play (numbers : int array) (turns : int) =
+    let tryFirstIndexOfRev num numbers =
+        numbers
+        |> Array.rev
+        |> Array.tryFindIndex ((=) num)
+
+    let next numbers =
+        let num = numbers |> Array.last
+        match tryFirstIndexOfRev num numbers.[..numbers.Length-2] with
+        | None -> 0
+        | Some i -> i + 1
+
+    let rec loop (numbers : int array) =
+        if numbers.Length >= turns then
+            numbers
+        else
+            let num = next numbers
+            loop (Array.append numbers [|num|])
+
+    loop numbers
+
+
+
---
+++ AdventOfCode2020/Day 15 - Rambunctious Recitation/MemoryTests.fs
@@ -0,0 +1,47 @@
+﻿module MemoryTests
+
+open FsUnit.Xunit
+open Xunit
+open Memory
+open Xunit.Abstractions
+
+type MemoryTests(output: ITestOutputHelper) =
+
+    [<Fact>]
+    member __.``Example Data`` () =
+        let input = [| 0 ; 3 ; 6 |]
+
+        let r = play input 2020
+
+        r |> Array.length |> should equal 2020
+        r |> Array.last |> should equal 436
+
+    [<Fact>]
+    member __.``Puzzle Input`` () =
+        let input = [| 1 ; 20 ; 11 ; 6 ; 12 ; 0 |]
+
+        let r = play input 2020
+
+        r |> Array.length |> should equal 2020
+
+        output.WriteLine("{0}", r |> Array.last)
+
+
+    [<Fact>]
+    member __.``Example Data - Part II`` () =
+        let input = [| 0 ; 3 ; 6 |]
+
+        let r = play input 30000000
+
+        r |> Array.length |> should equal 30000000
+        r |> Array.last |> should equal 175594
+
+    [<Fact>]
+    member __.``Puzzle Input - Part II`` () =
+        let input = [| 1 ; 20 ; 11 ; 6 ; 12 ; 0 |]
+
+        let r = play input 30000000
+
+        r |> Array.length |> should equal 30000000
+
+        output.WriteLine("{0}", r |> Array.last)
