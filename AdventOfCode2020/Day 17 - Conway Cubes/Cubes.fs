06e7ce025e32f491feb1ea0402f0652f5badef52
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-17 23:03:20 +0200

    Day 17 - Part II

--- AdventOfCode2020/Day 17 - Conway Cubes/Cubes.fs
+++ AdventOfCode2020/Day 17 - Conway Cubes/Cubes.fs
@@ -1,5 +1,6 @@
 ﻿module Cubes
 open System.Collections.Generic
+open System.Collections.Concurrent
 
 type State =
     | Active
@@ -26,12 +27,73 @@ let scan (exp : string []) (cycles : int) =
         |> dict
 
     let rec loop (map : IDictionary<_,_>) (cycle : int) =
-        // TODO: Should be ConcurrentDictionary
-        let dict = Dictionary<(int*int*int),State>()
+        let dict = ConcurrentDictionary<(int*int*int),State>()
         map.Keys
         |> Seq.toArray
         |> Array.collect nearby
-        |> Array.iter (fun current -> // TODO: Should be Array.Parallel.iter
+        |> Array.Parallel.iter (fun current ->
+            let activeNearby =
+                current
+                |> nearby
+                |> Array.map (fun n ->
+                    match map.TryGetValue(n) with
+                    | (false, _) -> Inactive
+                    | (true, s) -> s
+                )
+                |> Array.filter (fun s -> s = Active)
+                |> Array.length
+
+            let currentState =
+                match map.TryGetValue(current) with
+                | (false, _) -> Inactive
+                | (true, s) -> s
+
+            match (currentState, activeNearby) with
+            | (Active, 2) | (Active, 3) -> dict.[current] <- Active
+            | (Active, _) -> dict.[current] <- Inactive
+            | (Inactive, 3) -> dict.[current] <- Active
+            | (Inactive, _) -> dict.[current] <- Inactive
+        )
+
+        match cycle + 1 with
+        | c when c = cycles -> dict
+        | c -> loop dict c
+
+
+    let result = loop initial 0
+
+    result.Values
+    |> Seq.filter (fun s -> s = Active)
+    |> Seq.length
+
+let scan2 (exp : string []) (cycles : int) =
+    let nearby (x,y,z,w) =
+        [|x-1..x+1|]
+        |> Array.collect (fun x ->
+            [|y-1..y+1|]
+            |> Array.collect (fun y ->
+                [|z-1..z+1|]
+                |> Array.collect (fun z ->
+                    [|w-1..w+1|]
+                    |> Array.map (fun w -> (x,y,z,w)))))
+        |> Array.except [|(x,y,z,w)|]
+
+    let initial = 
+        exp
+        |> Array.mapi (fun y str ->
+            str.ToCharArray()
+            |> Array.mapi (fun x c ->
+                let state = if c = '#' then Active else Inactive
+                ((x, y, 0, 0), state)))
+        |> Array.collect id
+        |> dict
+
+    let rec loop (map : IDictionary<_,_>) (cycle : int) =
+        let dict = ConcurrentDictionary<(int*int*int*int),State>()
+        map.Keys
+        |> Seq.toArray
+        |> Array.collect nearby
+        |> Array.Parallel.iter (fun current ->
             let activeNearby =
                 current
                 |> nearby
--- AdventOfCode2020/Day 17 - Conway Cubes/CubesTests.fs
+++ AdventOfCode2020/Day 17 - Conway Cubes/CubesTests.fs
@@ -35,3 +35,32 @@ type CubesTests(output: ITestOutputHelper) =
         let active = scan notes 6
 
         output.WriteLine("Active {0}", active)
+
+    [<Fact>]
+    member __.``Example Data - Part II`` () =
+        let notes = [|
+            ".#."
+            "..#"
+            "###"
+        |]
+
+        let active = scan2 notes 6
+
+        active |> should equal 848
+
+    [<Fact>]
+    member __.``Puzzle Input - Part II`` () =
+        let notes = [|
+            "##..####"
+            ".###...."
+            "#.###.##"
+            "#....#.."
+            "...#..#."
+            "#.#...##"
+            "..#.#.#."
+            ".##...#."
+        |]
+
+        let active = scan2 notes 6
+
+        output.WriteLine("Active {0}", active)
