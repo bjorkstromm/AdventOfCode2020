c9cf051922c064abad12a2fb49685de58debbe7c
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-12-18 20:56:07 +0200

    Day 18 - Part I

---
+++ AdventOfCode2020/Day 18 - Operation Order/Operation.fs
@@ -0,0 +1,26 @@
+﻿module Operation
+
+open System
+
+let calc (exp : string) =
+    let rec loop state op lst =
+        match lst with
+        | [] -> (state, [])
+        | char::tail ->
+            match (char, Int64.TryParse(char.ToString())) with
+            | (' ', _) -> tail |> loop state op
+            | ('+', _) -> tail |> loop state (+)
+            | ('*', _) -> tail |> loop state (*)
+            | ('(', _) ->
+                let (inner, lst) = tail |> loop 0L (+)
+                lst |> loop (state |> op inner) op
+            | (')', _) -> (state, tail)
+            | (_, (true, n)) -> tail |> loop (state |> op n) op
+            | (c, (_,_)) -> failwithf "Invalid character %c" c
+
+    let (sum, _) =
+        exp.ToCharArray()
+        |> Array.toList
+        |> loop 0L (+)
+
+    sum
